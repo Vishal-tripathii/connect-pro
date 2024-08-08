e32b68c50bc9d1604972a3d6bdef60464a3928d1
vishal <rdshrma61@gmail.com>
2024-08-08 12:12:27 +0530

    refactored the like logic according to UI changes as well added visible css changes

--- src/app/components/partials/post-card/post-card.component.ts
+++ src/app/components/partials/post-card/post-card.component.ts
@@ -11,17 +11,26 @@ export class PostCardComponent implements OnInit {
 
   @Input() post!: any;
   currentUser!: any;
+  isPostLiked!: boolean;
+  likesCount!: any;
 
   constructor(private _feedService: FeedService, private _userService: UserService) {
     this.currentUser = this._userService.getCurrentUser();
   }
   ngOnInit(): void {
+    const index = this.post.likes.findIndex((item: any) => item.userId === this.currentUser._id);
+    if (index !== -1) {
+      this.isPostLiked = true;
+    }
+    this.likesCount = this.post.likes.length;
   }
 
   likeAction(postId: string) {
     this._feedService.likeAction(postId, this.currentUser?._id).subscribe({
       next: (like) => {
         console.log(like, "liked post sucessfully");
+        this.isPostLiked = true;
+        this.likesCount++;
       },
       error: (err) => {
         console.log(err, "err in liking post");
